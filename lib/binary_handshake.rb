586264e2612dc2ee7fc06fe3064679476ec31520
Chris Aporta <christopher.aporta@gmail.com>
2015-10-22 22:48:32 -0400

    complete lab

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -6,4 +6,28 @@
 # 1000 = "jump"
 # 10000 = Reverse the order of the operations in the secret handshake.
 
-# Write your code here.
+class SecretHandshake
+  def initialize(bin)
+    @bin_arr = bin.split("")
+  end
+
+  def commands
+    handshake = []
+    if @bin_arr[-1] == "1"
+      handshake << "wink"
+    end
+    if @bin_arr[-2] == "1"
+      handshake << "double blink"
+    end
+    if @bin_arr[-3] == "1"
+      handshake << "close your eyes"
+    end
+    if @bin_arr[-4] == "1"
+      handshake << "jump"
+    end
+    if @bin_arr[-5] == "1"
+      handshake.reverse!
+    end
+    handshake
+  end
+end
